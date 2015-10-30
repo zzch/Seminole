2d7013f03231f4416d1e4c91a2ca04acaee84344
seairy <seairy@gmail.com>
2015-10-30 16:01:37 +0800

    init commit

--- app/controllers/op/profiles_controller.rb
+++ app/controllers/op/profiles_controller.rb
@@ -2,13 +2,13 @@
 class Op::ProfilesController < Op::BaseController
   
   def edit
-    @administrator = Administrator.find(session['administrator']['id'])
+    @operator = @current_club.operators.find(session['operator']['id'])
   end
   
   def update
-    @administrator = Administrator.find(session['administrator']['id'])
-    if @administrator.update(profile_params)
-      session['administrator']['name'] = @administrator.name
+    @operator = @current_club.operators.find(session['operator']['id'])
+    if @operator.update(profile_params)
+      session['operator']['name'] = @operator.name
       redirect_to edit_cms_profile_path, notice: '操作成功！'
     else
       render action: 'edit'
@@ -16,21 +16,21 @@ class Op::ProfilesController < Op::BaseController
   end
   
   def update_password
-    administrator = Administrator.find(session['administrator']['id'])
-    if not administrator.authenticate(params[:orginal_password])
+    operator = @current_club.operators.find(session['operator']['id'])
+    if not operator.authenticate(params[:orginal_password])
       redirect_to edit_password_cms_profile_path, alert: '修改失败！原密码不正确！'
     elsif not /^[A-Za-z0-9]{6,16}$/ =~ params[:new_password]
       redirect_to edit_password_cms_profile_path, alert: '修改失败！新密码应由6至16位的大小写字母及数字组成，请重试！'
     elsif not params[:new_password] == params[:password_confirmation]
       redirect_to edit_password_cms_profile_path, alert: '修改失败！两次新密码输入不一致！'
     else
-      administrator.update_password params[:new_password]
+      operator.update_password params[:new_password]
       redirect_to edit_password_cms_profile_path, notice: '修改成功！'
     end
   end
 
   protected
-  def profile_params
-    params.require(:administrator).permit(:name)
-  end
+    def profile_params
+      params.require(:operator).permit(:name)
+    end
 end
