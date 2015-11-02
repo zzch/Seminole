4bfdb721603d45917a35f7211d131b9f7b583b23
seairy <seairy@gmail.com>
2015-11-02 17:34:09 +0800

    init commit

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,21 +3,21 @@ class Weather < ActiveRecord::Base
   scope :recently, ->(started_at, days) { where('date >= ?', started_at.to_date).where('date < ?', (started_at + days.days).to_date) }
 
   def code
-    if %w(0 1 2 3).include?(self.day_code.to_s)
+    if %w(0 1 2 3).include?(self.day_code.to_i)
       1
-    elsif %w(5 6).include?(self.day_code.to_s)
+    elsif %w(5 6).include?(self.day_code.to_i)
       2
-    elsif %w(4 7 8).include?(self.day_code.to_s)
+    elsif %w(4 7 8).include?(self.day_code.to_i)
       3
-    elsif %w(9).include?(self.day_code.to_s)
+    elsif %w(9).include?(self.day_code.to_i)
       4
-    elsif %w(10 11 12 13 14 15 16 17 18 19 20).include?(self.day_code.to_s)
+    elsif %w(10 11 12 13 14 15 16 17 18 19 20).include?(self.day_code.to_i)
       5
-    elsif %w(21 22 23 24 25).include?(self.day_code.to_s)
+    elsif %w(21 22 23 24 25).include?(self.day_code.to_i)
       6
-    elsif %w(30).include?(self.day_code.to_s)
+    elsif %w(30).include?(self.day_code.to_i)
       7
-    elsif %w(31).include?(self.day_code.to_s)
+    elsif %w(31).include?(self.day_code.to_i)
       8
     else
       1
