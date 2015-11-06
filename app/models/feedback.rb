f8be423506ac32919a18030340f99f040b35da86
seairy <seairy@gmail.com>
2015-11-06 16:46:22 +0800

    init commit

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,6 @@
 class Feedback < ActiveRecord::Base
   include UUID
-  as_enum :type, [:manager, :receiption, :restaurant], prefix: true, map: :string
+  as_enum :type, [:manager, :reception, :restaurant], prefix: true, map: :string
   belongs_to :club
   belongs_to :user
 end
