6b55340004bcd16bfa50014a8d419f823e1b7bd5
seairy <seairy@gmail.com>
2015-11-13 16:43:06 +0800

    init commit

--- app/models/operator_behavior.rb
+++ app/models/operator_behavior.rb
@@ -2,7 +2,7 @@ class OperatorBehavior < ActiveRecord::Base
   belongs_to :operator
   as_enum :type, [:generate, :sign_in, :service_process], map: :string
 
-  self.contents.keys.each do |content|
+  self.types.keys.each do |content|
     define_singleton_method "#{type}!" do |content|
       create!(type: type, content: content)
     end
