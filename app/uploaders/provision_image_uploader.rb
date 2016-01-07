e3b69240105d8e0a8703a6a2beae66d61b8d614b
seairy <seairy@gmail.com>
2016-01-07 14:29:05 +0800

    init commit

--- app/uploaders/provision_image_uploader.rb
+++ app/uploaders/provision_image_uploader.rb
@@ -1,12 +1,7 @@
 # -*- encoding : utf-8 -*-
 class ProvisionImageUploader < BaseUploader
-  version :w800_h800_fl_q80 do
+  version :w290_h220_fl_q80 do
     process quality: 80
-    process resize_to_fill: [800, 800]
-  end
-
-  version :w150_h150_fl_q50 do
-    process quality: 50
-    process resize_to_fill: [150, 150]
+    process resize_to_fill: [290, 220]
   end
 end
