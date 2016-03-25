50e09deaaf5120f5241f9857439fef9121fb7ddf
seairy <seairy@gmail.com>
2016-03-25 18:40:21 +0800

    init commit

--- app/apis/v1/members_api.rb
+++ app/apis/v1/members_api.rb
@@ -20,9 +20,9 @@ module V1
         expose :tab, using: Tab
         expose :amount do |m, o|
           case m.member.card.type
-          when :by_ball then "#{m.amount / m.member.club.balls_per_bucket}筐球"
-          when :by_time then "#{m.amount}分钟"
-          when :stored then "#{sprintf('%0.02f', m.amount)}元"
+          when :by_ball then "#{(m.amount / m.member.club.balls_per_bucket).round}筐球"
+          when :by_time then "#{m.amount.round}分钟"
+          when :stored then "#{m.amount.round}元"
           else 'N/A'
           end
         end
